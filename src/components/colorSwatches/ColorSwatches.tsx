ee40369968dee8826c318c75d98d3502f26c2076
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-09-17 08:57:16 -0700

    add change bg color body function

--- src/components/colorSwatches/ColorSwatches.tsx
+++ src/components/colorSwatches/ColorSwatches.tsx
@@ -3,24 +3,29 @@ import './ColorSwatches.css'
 
 const ColorSwatches = () => {
     const [colors, setColors] = useState<string[]>([])
-    const [color, setColor] = useState<string>('')
-
+    const [colorInput, setColorInput] = useState<string>('')
 
     const addColor = () => {
-        if (!color) {
+        if (!colorInput) {
             alert('Please enter a valid color')
+            return
         }
-        setColors([...colors, color])
-        setColor('')
+        setColors([...colors, colorInput])
+        setColorInput('')
+    }
+
+    const changeBgColor = (bg: string) => {
+        document.body.style.backgroundColor = bg
     }
 
     return (
         <section>
-            <input type="text" value={color} onChange={(e) => setColor(e.target.value)} />
+            <h1>Color Swatches</h1>
+            <input type="text" value={colorInput} onChange={(e) => setColorInput(e.target.value)} />
             <button className="" onClick={addColor}>Add color</button>
             <div className="color-picker__container">
                 {colors.map((color, index) =>
-                    <div key={index} className="color-opt" style={{background: `${color}`}}>
+                    <div key={index} className="color-opt" style={{ background: `${color}` }} onClick={() => changeBgColor(color)}>
                         <p>{color}</p>
                     </div>
                 )}
